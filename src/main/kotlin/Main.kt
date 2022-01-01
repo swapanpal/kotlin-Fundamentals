0168a708a7c3df17dec78ef488ba53ab17fc29d4
swapan <swapan.d83@gmail.com>
2022-01-02 00:16:22 +0600

    function example: with List<Int> as parameter

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,29 @@
 /**
- * simplified declaration of function with input and return value
+ * pass 'List' as a parameter of function
+ * The list look like as  : [2,3,5,6,7,8,9,4,3]
+ * Search a number from the list by typing the number
  */
 fun main(){
+    val list = listOf(3,2,5,1,5,6,7)
+    println("The list looks like the following: $list")
+    println("Search for this number: ")
 
+    // Take an input
+    val input = readLine()?.toInt()
 
-    // call the second function(simplified function)
-   val result2 =  multiplyTwoNumber(5, 3)
-    println(" Result of two number is $result2")
+    // Check the given number is not null
+    if (input != null){
+        println("The index of $input is ${indexOf(list, input)}")
+    }
+
 
 }
-
-// simplified function declaration
-// This function take two input as Int and return an Int value
-fun multiplyTwoNumber(a : Int, b : Int) = a * b
+// Create the function that take a list as parameters and return an Int value
+fun indexOf(list : List<Int>, value : Int) : Int{
+    for (i in 0..list.size - 1){
+        if (list[i] == value){
+            return i
+        }
+    }
+    return -1
+}
