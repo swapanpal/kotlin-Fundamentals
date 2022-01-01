7526945180d8cc5e35c505c8a80f54a5317d0869
swapan <swapan.d83@gmail.com>
2022-01-01 20:49:52 +0600

    Example-2 of kotlin 'when' keyword

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,16 @@
 /**
- * Usage of User input, when() and range
+ * Example-2 of kotlin 'when'
  */
 fun main(){
-    println("Enter your age: ")
-    // take an input as Integer
-       val age = readLine()?.toInt()
+    println("Where are you from?")
+    // take userInput
+    val country = readLine()
 
-    when(age){
-        in 0..5 -> println("You're a young kid")
-        in 6..17 -> println("You're a teenager")
-        18 -> println("Finally, you're 18")
-        19,20 -> println("You're a young adult")
-        in 21..65 -> println("You're an adult")
-        else -> println("You're really old")
+    when(country){
+        "India" -> println("Namaste")
+        "USA" -> println("Hello")
+        "Germany" -> println("Hallo")
+        "Russia" -> println("Privet")
+        else -> println("I don't know that")
     }
 }
